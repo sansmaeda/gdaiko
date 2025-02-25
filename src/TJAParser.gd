eced466bd2341e220f2b275a3846b7b814918c46
sansmaeda <sansmaeda.dev@gmail.com>
2025-02-25 09:26:00 -0500

    Added subtitle parser and fixed parser trimming

--- src/TJAParser.gd
+++ src/TJAParser.gd
@@ -25,12 +25,13 @@ var SubtitleCN: String
 var SubtitleTW: String
 var SubtitleKO: String
 
-##Initial BPM of the song.
+##Initial BPM of the song. Can be changed mid-song.
 var BPM: float = 120
 ##Path to the song audio file used during gameplay. Located in the same folder as the TJA.
 var Wave: String
 ##Offset of the notes in seconds.
 var Offset: float 
+##Offset of the title gameplay demonstration for the song
 var DemoStart: float
 
 var Genre: String
@@ -85,39 +86,82 @@ func parse(path: String):
 	#Title
 	regex.compile("(?m)^TITLE:.*$")
 	buffer = regex.search(text)
-	if(buffer): Title = buffer.get_string()
+	if(buffer): Title = buffer.get_string().trim_prefix("TITLE:")
 	buffer = null
 	
 	#TitleJP
 	regex.compile("(?m)^TITLEJA:.*$")
 	buffer = regex.search(text)
-	if(buffer): TitleJP = buffer.get_string()
+	if(buffer): TitleJP = buffer.get_string().trim_prefix("TITLEJA:")
 	buffer = null
 	
 	#TitleEN
 	regex.compile("(?m)^TITLEEN:.*$")
 	buffer = regex.search(text)
-	if(buffer): TitleEN = buffer.get_string()
+	if(buffer): TitleEN = buffer.get_string().trim_prefix("TITLEEN:")
 	buffer = null
 	
 	#TitleCN
 	regex.compile("(?m)^TITLECN:.*$")
 	buffer = regex.search(text)
-	if(buffer): TitleCN = buffer.get_string()
+	if(buffer): TitleCN = buffer.get_string().trim_prefix("TITLECN:")
 	buffer = null
 	
 	#TitleTW
 	regex.compile("(?m)^TITLETW:.*$")
 	buffer = regex.search(text)
-	if(buffer): TitleTW = buffer.get_string()
+	if(buffer): TitleTW = buffer.get_string().trim_prefix("TITLETW:")
 	buffer = null
 	
 	#TitleKO
 	regex.compile("(?m)^TITLEKO:.*$")
 	buffer = regex.search(text)
-	if(buffer): TitleKO = buffer.get_string()
+	if(buffer): TitleKO = buffer.get_string().trim_prefix("TITLEKO:")
 	buffer = null
 	#endregion
+	
+	#region Subtitles
+	#Subtitle
+	regex.compile("(?m)^SUBTITLE:.*$")
+	buffer = regex.search(text)
+	if(buffer): Subtitle = buffer.get_string().trim_prefix("SUBTITLE:")
+	buffer = null
+	
+	#SubtitleJP
+	regex.compile("(?m)^SUBTITLEJA:.*$")
+	buffer = regex.search(text)
+	if(buffer): SubtitleJP = buffer.get_string().trim_prefix("SUBTITLEJA:")
+	buffer = null
+	
+	#SubtitleEN
+	regex.compile("(?m)^SUBTITLEEN:.*$")
+	buffer = regex.search(text)
+	if(buffer): SubtitleEN = buffer.get_string().trim_prefix("SUBTITLEEN:")
+	buffer = null
+	
+	#SubtitleCN
+	regex.compile("(?m)^SUBTITLECN:.*$")
+	buffer = regex.search(text)
+	if(buffer): SubtitleCN = buffer.get_string().trim_prefix("SUBTITLECN:")
+	buffer = null
+	
+	#SubtitleTW
+	regex.compile("(?m)^SUBTITLETW:.*$")
+	buffer = regex.search(text)
+	if(buffer): SubtitleTW = buffer.get_string().trim_prefix("SUBTITLETW:")
+	buffer = null
+	
+	#SubtitleKO
+	regex.compile("(?m)^SUBTITLEKO:.*$")
+	buffer = regex.search(text)
+	if(buffer): SubtitleKO = buffer.get_string().trim_prefix("SUBTITLEKO:")
+	buffer = null
+	#endregion
+	
+	#BPM
+	regex.compile("(?m)^BPM:.*$")
+	buffer = regex.search(text)
+	if(buffer): BPM = buffer.get_string().trim_prefix("BPM:").to_float()
 	
 	print(Title)
 	print(TitleJP)
@@ -128,3 +172,9 @@ func parse(path: String):
 	print("")
 	print(Subtitle)
 	print(SubtitleJP)
+	print(SubtitleEN)
+	print(SubtitleCN)
+	print(SubtitleTW)
+	print(SubtitleKO)
+	print("")
+	print(BPM)
