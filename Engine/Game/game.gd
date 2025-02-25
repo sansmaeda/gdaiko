a788b900e6fa991be78306342ff5599c51a7f3f5
sansmaeda <sansmaeda.dev@gmail.com>
2025-02-24 22:52:14 -0500

    Added gitignore

---
+++ Engine/Game/game.gd
@@ -0,0 +1,16 @@
+extends Node
+var title: String
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func parse_tja(path: String):
+	#https://github.com/269Seahorse/Better-taiko-web/blob/master/TJA-format.mediawiki#user-content-TITLEEN_i
+	pass
