435b835420a8f408888af68b7d791704b73ab031
sansmaeda <sansmaeda.dev@gmail.com>
2025-03-04 15:37:39 -0500

    Correction

--- Themes/Default Theme/Song Select/song_select.gd
+++ Themes/Default Theme/Song Select/song_select.gd
@@ -2,16 +2,25 @@ extends Node2D
 class_name SongSelect
 
 var genres: Array[String]
-var selected_genre: int = 0
-var selected_song: int = 0
+var selected_genre: int = 0:
+	set(value):
+		if(value < 0): selected_genre = genres.size()-1 - value
+		elif(value > genres.size()-1): selected_genre = 0
+		else: selected_genre = value
+var selected_song: int = 0:
+	set(value):
+		if(value < 0): selected_song = song_list.size()-1 - value
+		elif(value > song_list.size()-1): selected_song = 0
+		else: selected_song = value
+#Represents whether the menu is in genre or song selector
 var focused: bool = false
+var song_list: Array[String]
 
 func _ready():
 	var folders = DirAccess.get_directories_at("user://Songs/")
 	for folder in folders:
 		if FileAccess.file_exists("user://Songs/".path_join(folder).path_join("box.def")):
 			genres.append("user://Songs/".path_join(folder))
-	print(genres)
 
 func _process(_delta):
 	if !focused:
@@ -19,18 +28,14 @@ func _process(_delta):
 			selected_genre -= 1
 		if(Input.is_action_just_pressed("ka_right")):
 			selected_genre += 1
-		if(selected_genre < 0): selected_genre = genres.size()-1
-		if(selected_genre > genres.size()-1): selected_genre = 0
 		$"Header 4 (Selected)/Label".text = get_genre_text(genres[selected_genre])
 		if Input.is_action_just_pressed("don_right"): focused = true
-	elif focused:
-		var song_list: Array[String] = get_songs(genres[selected_genre])
+	else:
+		song_list = get_songs(genres[selected_genre])
 		if(Input.is_action_just_pressed("ka_left")):
 			selected_song -= 1
 		if(Input.is_action_just_pressed("ka_right")):
 			selected_song += 1
-		if(selected_song < 0): selected_song = genres.size()-1
-		if(selected_song > genres.size()-1): selected_song = 0
 		var parser: TJAParser = TJAParser.new()
 		parser.parse(song_list[selected_song])
 		$"Header 4 (Selected)/Label".text = parser.title
